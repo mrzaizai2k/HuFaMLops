ea0860b50bb35f44da2d70bf45e2aaa4eef3a8bf
Mai Chí Bảo <mrzaizai2k@gmail.com>
2023-06-14 09:31:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 from transformers import pipeline
 import gradio as gr
-# I add cmt
 
-# Initialize the summarization model
-model = pipeline("summarization")
 
+model = pipeline(
+    "summarization",
+)
 
 def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
 
-# Create a Gradio interface
-interface = gr.Interface(fn=predict, inputs=gr.Textbox(placeholder="Enter text to summarize", lines=4), outputs="text")
-
-
-# Launch the interface
-interface.launch()
-
+# create an interface for the model
+with gr.Interface(predict, "textbox", "text") as interface:
+    interface.launch()
