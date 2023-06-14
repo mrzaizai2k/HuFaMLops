f5611c9d399ef442c9aef9614589368913da3d7c
Mai Chí Bảo <mrzaizai2k@gmail.com>
2023-06-14 02:55:12 +0000

    app change

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 from transformers import pipeline
 import gradio as gr
 
-<<<<<<< HEAD
-# I add a comt
-# Initialize the summarization model
-model = pipeline("summarization")
-
-=======
-
 model = pipeline(
     "summarization",
 )
->>>>>>> refs/remotes/origin/main
 
 def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
-
 
 # create an interface for the model
 with gr.Interface(predict, "textbox", "text") as interface:
